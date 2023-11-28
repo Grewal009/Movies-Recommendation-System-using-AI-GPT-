693f6645322b9305ba5fd0c4f56a69d11882e59b
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-28 23:42:19 +0100

    Firebase setup and deploying app to production

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbtpGzr2ac8jeNVUx9rnRqDxyWRBJSV64",
+  authDomain: "netflixgpt-3b50d.firebaseapp.com",
+  projectId: "netflixgpt-3b50d",
+  storageBucket: "netflixgpt-3b50d.appspot.com",
+  messagingSenderId: "613147574676",
+  appId: "1:613147574676:web:7335bdf75a72da0d538f81",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
