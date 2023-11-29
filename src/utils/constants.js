d8f02270baffcae0463a2719ced0784ab064980a
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-29 13:04:33 +0100

    Get data from TMDB now playing movies list API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,12 @@ export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const BG_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/db6c3334-0569-4cf8-af49-e07405d94902/NO-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzBmM2FhNGQyOTVlNzhmZjA5ZGMzNjNmMjc1ZWU0ZiIsInN1YiI6IjY1NjcxZDE3YzJiOWRmMDEwMDRjYTg2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MaD2plSVRt82TQa0J7MvuEKnsBfST67vMiQ_aEYRWNY",
+  },
+};
