8ad96e1a6af073f8a9653fdc7e312e5fc933f5d7
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-29 19:16:45 +0100

    embedded youtube video and make it autoplay and mute

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,13 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
-      <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-[400px]">{overview}</p>
+    <div className="pt-[20%] px-14 absolute text-white bg-gradient-to-r from-black to-50% w-screen aspect-video">
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="py-6 text-base w-[450px]">{overview}</p>
       <div className="">
-        <button className="bg-slate-500 mx-2 text-white px-12 py-2 text-lg font-bold bg-opacity-50 rounded-lg">
+        <button className="bg-white mx-2 text-black px-12 py-2 text-lg font-bold bg-opacity-90 rounded-lg hover:bg-opacity-80">
           ▶️ Play
         </button>
-        <button className="bg-slate-500 text-white px-12 py-2 text-lg font-bold bg-opacity-50 rounded-lg mx-2">
+        <button className="bg-slate-500 text-white px-12 py-2 text-lg font-bold bg-opacity-80 rounded-lg mx-2">
           More Info
         </button>
       </div>
