0f039c0763a3b5a957275bde5d25863b709a476d
Aneel Grewal <aneel.grewal@hotmail.com>
2023-12-05 21:35:39 +0100

    Shimmer component created

---
+++ src/components/Shimmer.js
@@ -0,0 +1,41 @@
+const Shimmer = () => {
+  return (
+    <div className="p-1 md:p-2 bg-black text-white bg-opacity-70">
+      <div className="px-1 md:px-2">
+        <div className="bg-slate-300 w-28 h-5 mx-4 mb-2 mt-4 pt-2 md:pt-4"></div>
+        <div className="flex overflow-x-scroll">
+          <div className="flex">
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-1 md:px-2">
+        <div className="bg-slate-300 w-28 h-5 mx-4 mb-2 mt-4 pt-2 md:pt-4"></div>
+        <div className="flex overflow-x-scroll">
+          <div className="flex">
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+            <div className="w-[100px] md:w-40 pr-1 md:pr-2">
+              <div className="w-[96px] h-[144px] bg-slate-300 md:w-[152px] md:h-[228px]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
